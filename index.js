91c43319669136d619fafbdb0e44080c8b2d4da9
PavanKoshti <pavankoshti786@gmail.com>
2023-02-17 11:11:30 +0530

    SimpleServer

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const details =  require('./second')
+const express = require('express')
+const app = express()
+const port = 2410
 
-// console.log("Hello Node Js", details);
+app.get('/', (req, res) => {
+  res.send('<h1> THIS IS HOME PAGE </h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1> THIS IS HOME PAGE </h1>')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('<h1> THIS IS CONTACT PAGE </h1>')
+})
+
+app.get('/help', (req, res) => {
+    res.send('<h1> THIS IS HELP PAGE </h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- second.js
+++
@@ -1,7 +0,0 @@
-    pavan = {
-        name:"pavankuamr",
-        age: 22
-    }
-
-module.exports = pavan         
-
